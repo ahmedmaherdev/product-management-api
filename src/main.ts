4c02af2e0e385f7961a58667f35353b333020309
Ahmed Maher <ahmed.maher.sallam@vultara.com>
2024-11-26 22:12:54 +0200

    add helmet and enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.enableCors();
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
   
